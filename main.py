aa2d866e56519348be141aa835d6645f2b67af54
pierre-rerot <134595079+pierre-rerot@users.noreply.github.com>
2023-05-25 10:16:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
 
-# Function to display the game board
 def display_board(board):
     print("-------------")
     for i in range(3):
@@ -9,19 +8,15 @@ def display_board(board):
             print(board[i][j], "|", end=" ")
         print("\n-------------")
 
-# Function to check if a player has won
 def check_win(board, symbol):
-    # Check rows
     for i in range(3):
         if board[i][0] == board[i][1] == board[i][2] == symbol:
             return True
 
-    # Check columns
     for i in range(3):
         if board[0][i] == board[1][i] == board[2][i] == symbol:
             return True
 
-    # Check diagonals
     if board[0][0] == board[1][1] == board[2][2] == symbol:
         return True
     if board[0][2] == board[1][1] == board[2][0] == symbol:
@@ -29,10 +24,9 @@ def check_win(board, symbol):
 
     return False
 
-# Function for a player's turn
 def player_turn(board, symbol):
     while True:
-        move = input(f"Enter your move (row column): ")
+        move = input("Enter your move (row column): ")
         try:
             row, col = map(int, move.split())
             if 0 <= row <= 2 and 0 <= col <= 2 and board[row][col] == " ":
@@ -43,9 +37,7 @@ def player_turn(board, symbol):
         except ValueError:
             print("Invalid input. Try again.")
 
-# Function for the computer's turn
 def computer_turn(board, symbol):
-    # Check for a winning move
     for i in range(3):
         for j in range(3):
             if board[i][j] == " ":
@@ -55,7 +47,6 @@ def computer_turn(board, symbol):
                 else:
                     board[i][j] = " "
 
-    # Check for a blocking move
     for i in range(3):
         for j in range(3):
             if board[i][j] == " ":
@@ -66,7 +57,6 @@ def computer_turn(board, symbol):
                 else:
                     board[i][j] = " "
 
-    # Choose a random move
     while True:
         row = random.randint(0, 2)
         col = random.randint(0, 2)
@@ -74,7 +64,6 @@ def computer_turn(board, symbol):
             board[row][col] = symbol
             break
 
-# Function to play a game
 def play_game():
     while True:
         board = [[" " for _ in range(3)] for _ in range(3)]
@@ -98,5 +87,18 @@ def play_game():
 
             if check_win(board, symbol):
                 if symbol == player_symbol:
-                    print("Congratulations! You won!")
+                    print("Congratulations! You win!")
                 else:
+                    print("Sorry, you lose. Better luck next time.")
+                break
+
+            if " " not in board[0] and " " not in board[1] and " " not in board[2]:
+                print("It's a tie!")
+                break
+
+            turn += 1
+
+        play_again = input("Do you want to play again? (yes/no): ")
+        if play_again.lower() != "yes":
+          break
+play_game()
